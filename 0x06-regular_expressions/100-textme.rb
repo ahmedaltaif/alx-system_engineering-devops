f6997be48a0a9297bb61606112ed25a02a3473fa
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-02-04 22:29:33 +0300

    Regular expression

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #regular expression must match a 10 digit phone number
-puts ARGV[0].scan(/\[from:(.?*)\]\s\[to:(.?*)\]\s\[flags:(.?*)\]/).join
+puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
