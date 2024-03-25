ad5a4d2b084ed368ddd861f28fad09bbddd06d3d
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-26 01:49:50 +0300

    commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #install flask
-exec { 'install Flask':
-  command => 'sudo pip3 install flask==2.1.0',
-  path    => '/usr/bin'
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
