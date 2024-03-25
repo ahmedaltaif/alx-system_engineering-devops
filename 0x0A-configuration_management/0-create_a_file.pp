34c182909d7ae71c717658e965bba40feaa399c7
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-26 01:22:45 +0300

    commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # A resource declaration
-file { '/tmp/school':
-  ensure  => 'present',
-  mode    => '0744',
-  owner   => www-data,
-  group   => www-data,
-  content => 'I love Puppet'
+file {'/tmp/school':
+    ensure  => file,
+    path    => '/tmp/school'
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
 }
