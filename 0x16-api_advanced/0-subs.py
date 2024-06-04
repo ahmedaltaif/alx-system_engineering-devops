b68903c71730d8bda44bf0ba8854edc68e2a3a07
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-06-05 02:30:09 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import requests
 """
     a function that queries the Reddit API and
     returns the number of subscribers
 """
+import requests
 
 
 def number_of_subscribers(subreddit):
