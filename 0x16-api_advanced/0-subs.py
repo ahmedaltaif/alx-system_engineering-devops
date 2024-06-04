149e11ead3eb636a5f2c2caf2a9f6b828513acef
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-06-05 02:26:31 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ return the numer of subscribers """
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-agent': '0X16API_ADVANCED'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
+    try:
+        response = requests.get(url,
+                                headers={'User-agent': 'your bot 0.1'},
+                                allow_redirects=False).json()
+        return response['data'].get('subscribers')
+    except Exception:
         return 0
-    result = response.json().get("data")
-    return result.get("subscribers")
