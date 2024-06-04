16fba47057fd3c9f402ee7abca0d5ac998163055
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-06-05 02:29:56 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
+import requests
 """
     a function that queries the Reddit API and
     returns the number of subscribers
 """
-import requests
 
 
 def number_of_subscribers(subreddit):
     """ return the numer of subscribers """
-    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
-    try:
-        response = requests.get(url,
-                                headers={'User-agent': 'your bot 0.1'},
-                                allow_redirects=False).json()
-        return response['data'].get('subscribers')
-    except Exception:
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-agent': '0X16API_ADVANCED'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    result = response.json().get("data")
+    return result.get("subscribers")
