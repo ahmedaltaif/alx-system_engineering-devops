2a5e20f9819788e4039e1cd3e094cf77d2f302a0
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-06-05 03:39:14 +0300

    __pycache__

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,22 +5,18 @@ recursive function
 import requests
 
 
-s = requests.Session()
-s.headers.update({'User-Agent': '0X16API_ADVANCED'})
-s.allow_redirects = False
+def recurse(subreddit, hot_list=[], after=""):
+    """ Function recurse """
 
-
-def recurse(subreddit, hot_list=[]):
-    """recursively grab subreddit"""
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = s.get(url).json()
-    try:
-        for tit in response['data']['children']:
-            hot_list.append(tit['data']['title'])
-        if response['data']['after']:
-            s.params = {'after': response['data']['after']}
-            return recurse(subreddit, hot_list)
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'user-agent': '0X16API_ADVANCED'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        response = response.json()
+        for post in response.get('data').get('children'):
+            hot_list.append(post.get('data').get('title'))
+        if response.get('data').get('after'):
+            recurse(subreddit, hot_list)
         return hot_list
-    except Exception:
+    else:
         return None
