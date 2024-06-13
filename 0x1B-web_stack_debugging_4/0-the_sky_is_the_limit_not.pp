d6fd3c07d966cdd10e7afd1f77584f7727bae00d
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-06-13 22:28:19 +0300

    commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
 # This manuscript increases the amount of traffic an Nginx server can handle
 
-# increase the ULIMIT of the default file
+# Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-} ->
+}
 
-# restart Nginx
-exec { 'nginx-restart':
+# Restart Nginx
+-> exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
